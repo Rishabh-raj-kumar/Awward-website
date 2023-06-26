effd77dabfe4f8439ac21d25732ce45b016ea0eb
Rishabh <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2023-06-26 10:47:57 +0530

    initial commit

--- main.js
+++ main.js
@@ -5,102 +5,80 @@ const icon = document.querySelector("header span ");
 const computedRoot = document.querySelector(":root");
 const cs = getComputedStyle(computedRoot);
 const explore = document.querySelector(".box h1");
+const para = document.querySelector('.box p');
+const homeContainer = document.querySelector('#HOME .container');
+const storiesheading = document.querySelectorAll('.story h2');
 
-//detect touch device..
-function isTouch() {
-  try {
-    //it will not work on desktop..
-    document.createEvent("TouchEvent");
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-const movement = (e) => {
-  try {
-    var x = !isTouch() ? e.pageX : e.touches[0].pageX;
-    var y = !isTouch() ? e.pageY : e.touches[0].pageY;
-  } catch (e) {}
-
-  mouseFollower.style.top = `${y}px`;
-  mouseFollower.style.left = `${x}px`;
-  console.log(e.pageX, e.pageY);
-
-  //   console.log(mouseFollower.style.top);
-  //   console.log(mouseFollower.style.left);
-};
-
-document.addEventListener("mousemove", (e) => {
-  movement(e);
-});
-document.addEventListener("touchmove", (e) => {
-  movement(e);
-});
-
-document.querySelector(".box h1").addEventListener("mousemove", () => {
-  mouseFollower.style.width = `200px`;
-});
-document.querySelector(".box h1").addEventListener("touchmove", () => {
-  mouseFollower.style.width = `60px`;
-});
-document.querySelector(".box h1").addEventListener("mouseleave", () => {
-  mouseFollower.style.width = `60px`;
-});
-
-document.querySelector(".box p").addEventListener("mousemove", () => {
-  mouseFollower.style.background = `rgb(255, 187, 0)`;
-});
-anchorli.forEach((lists) => {
-  lists.addEventListener("mousemove", () => {
-    lists.querySelector("a").style.color = `hsla(91, 23%, 2%, 1)`;
-    mouseFollower.style.background = `rgb(255, 187, 0)`;
-  });
-});
-document.querySelector(".box p").addEventListener("mouseleave", () => {
-  mouseFollower.style.background = `rgba(191, 135, 30, 0.832)`;
-});
-anchorli.forEach((lists) => {
-  lists.addEventListener("mouseleave", () => {
-    lists.querySelector("a").style.color = `hsla(46, 13%, 84%, 1)`;
-    mouseFollower.style.background = `rgba(191, 135, 30, 0.832)`;
-  });
-});
 
 let i = 0;
 icon.addEventListener("click", () => {
   // console.log(cs.getPropertyValue('--bg-blck'));
   i = i + 1;
-  if (i % 2 !== 0) {
+  if (i % 2 === 0) {
     icon.innerHTML = `<i class="fa-solid fa-lightbulb"></i>`;
     icon.querySelector('i').style.color = `hsla(91, 23%, 2%, 1)`;
 
+   homeContainer.style.setProperty('--forg-color','hsla(91, 23%, 2%, 1)');
+
     document.body.style.setProperty("--bg-blck", `hsla(46, 13%, 84%, 1)`);
-    explore.style.setProperty("--bg-lig-orange", `hsla(91, 23%, 2%, 1)`);
+    explore.style.setProperty("--forg-color", `hsla(46, 64%, 47%, 1)`);
     explore.style.mixBlendMode = "normal";
+
+    para.style.setProperty("--forg-color",`hsla(91, 23%, 2%, 1)`);
 
     anchorli.forEach((lists) => {
       lists.querySelector("a").style.color = `hsla(91, 23%, 2%, 1)`;
       lists.querySelector("a").style.mixBlendMode = "normal";
-      lists.addEventListener('mouseleave',() =>{
-        lists.querySelector("a").style.color = `hsla(91, 23%, 2%, 1)`;
-      });
     });
+
+    storiesheading.forEach((stories) =>{
+      stories.style.setProperty('--forg-color','hsla(91, 23%, 2%, 1)');
+    })
+
   } else {
     icon.innerHTML = `<i class="fa-solid fa-moon"></i>`;
     icon.querySelector('i').style.color = `hsla(46, 13%, 84%, 1)`;
 
-    document.body.style.setProperty("--bg-blck", `hsla(91, 23%, 2%, 1)`);
-    explore.style.setProperty("--bg-lig-orange", `hsla(46, 64%, 47%, 1)`);
+    homeContainer.style.setProperty('--forg-color','hsla(46, 13%, 84%, 1)');
 
+    document.body.style.setProperty("--bg-blck", `hsla(91, 23%, 2%, 1)`);
+    explore.style.setProperty("--forg-color", `rgb(255, 187, 0)`);
     explore.style.mixBlendMode = "difference";
+
+    para.style.setProperty("--forg-color",`hsla(46, 13%, 84%, 1)`);
+    para.style.mixBlendMode = "difference";
 
     anchorli.forEach((lists) => {
       lists.querySelector("a").style.color = `hsla(46, 13%, 84%, 1)`;
       lists.querySelector("a").style.mixBlendMode = "difference";
-      lists.addEventListener('mouseleave',() =>{
-        lists.querySelector("a").style.color = `hsla(46, 13%, 84%, 1)`;
-      });
     });
+
+    storiesheading.forEach((stories) =>{
+      stories.style.setProperty('--forg-color','hsla(37, 91%, 44%, 1)');
+    })
+
   }
 });
+
+//setup locomotive scroll
+const scroller = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
+
+// //animating website with gsap..
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.from('.box h1',{
+//   scrollTrigger : {
+//     trigger : '.box',
+//     start : `top 0px`,
+//     markers : true,
+//     toggleActions : "play none none none",
+//     end : `top 100px`,
+//     scrub : 1
+//   },
+//   opacity : 0,
+//    scale : 20,
+//   }
+// )
