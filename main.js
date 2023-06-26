d6e2e9dd2a15dd631253c6106cca85d754073139
Rishabh <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2023-06-26 11:59:31 +0530

    updated code

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const anchorli = document.querySelectorAll("header nav ul li");
 const icon = document.querySelector("header span ");
 const computedRoot = document.querySelector(":root");
 const cs = getComputedStyle(computedRoot);
-const explore = document.querySelector(".box h1");
+const explore = document.querySelectorAll(".box h1");
 const para = document.querySelector('.box p');
 const homeContainer = document.querySelector('#HOME .container');
 const storiesheading = document.querySelectorAll('.story h2');
-
+const storiesIcon = document.querySelector('#STORIES span');
+const spanWheel = document.querySelectorAll('.lerp-wrap span');
 
 let i = 0;
 icon.addEventListener("click", () => {
@@ -20,11 +21,17 @@ icon.addEventListener("click", () => {
 
    homeContainer.style.setProperty('--forg-color','hsla(91, 23%, 2%, 1)');
 
-    document.body.style.setProperty("--bg-blck", `hsla(46, 13%, 84%, 1)`);
-    explore.style.setProperty("--forg-color", `hsla(91, 23%, 2%, 1)`);
-    explore.style.mixBlendMode = "normal";
+    document.body.style.setProperty("--bg-white", `hsla(46, 13%, 84%, 1)`);
+    explore.forEach((exp) => {
+      exp.style.setProperty("--forg-color", `hsla(91, 23%, 2%, 1)`);
+    })
+
+    spanWheel.forEach((span) =>{
+      span.style.setProperty("--forg-color", `hsla(91, 23%, 2%, 1)`);
+    })
 
     para.style.setProperty("--forg-color",`hsla(91, 23%, 2%, 1)`);
+    storiesIcon.style.setProperty('--forg-color',`hsla(91, 23%, 2%, 1)`);
 
     anchorli.forEach((lists) => {
       lists.querySelector("a").style.color = `hsla(91, 23%, 2%, 1)`;
@@ -41,12 +48,19 @@ icon.addEventListener("click", () => {
 
     homeContainer.style.setProperty('--forg-color','hsla(46, 13%, 84%, 1)');
 
-    document.body.style.setProperty("--bg-blck", `hsla(91, 23%, 2%, 1)`);
-    explore.style.setProperty("--forg-color", `rgb(255, 187, 0)`);
-    explore.style.mixBlendMode = "difference";
+    document.body.style.setProperty("--bg-white", `hsla(91, 23%, 2%, 1)`);
+    explore.forEach((exp) => {
+      exp.style.setProperty("--forg-color", `rgb(255,187,0)`);
+    })
+
+    spanWheel.forEach((span) =>{
+      span.style.setProperty("--forg-color", `rgb(255,187,0)`);
+    })
 
     para.style.setProperty("--forg-color",`hsla(46, 13%, 84%, 1)`);
     para.style.mixBlendMode = "difference";
+
+    storiesIcon.style.setProperty('--forg-color',`rgb(255,187,0)`);
 
     anchorli.forEach((lists) => {
       lists.querySelector("a").style.color = `hsla(46, 13%, 84%, 1)`;
@@ -54,7 +68,7 @@ icon.addEventListener("click", () => {
     });
 
     storiesheading.forEach((stories) =>{
-      stories.style.setProperty('--forg-color','hsla(37, 91%, 44%, 1)');
+      stories.style.setProperty('--forg-color',`hsla(46, 13%, 84%, 1)`);
     })
 
   }
@@ -63,7 +77,10 @@ icon.addEventListener("click", () => {
 //setup locomotive scroll
 const scroller = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
-  smooth: true
+  smooth: true,
+  smartphone: {
+    smooth: true
+}
 });
 
 // //animating website with gsap..
